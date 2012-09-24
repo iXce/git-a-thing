09711b65f21028f206de3a771ffec7cdf2b14d25
Guillaume Seguin <guillaume@segu.in>
2012-09-24 12:26:45 +0200

    Initial models

--- gitathing_web/models.py
+++ gitathing_web/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    """User profile"""
+    user = models.ForeignKey(User)
+    display_name = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 200)
+    location = models.CharField(max_length = 100)
+    about = models.TextField("About me")
+
+class Media(models.Model):
+    """Static content not handled in git repositories"""
+    design = models.ForeignKey("Design", related_name = "media")
+    file = models.FileField(upload_to = "%Y/%m/%d/")
+    added = models.DateTimeField(auto_now_add = True)
+    uploader = models.ForeignKey(User)
+
+class License(models.Model):
+    """Design license"""
+    name = models.CharField(max_length = 150)
+    url = models.URLField(blank = True)
+    full_text = models.TextField(blank = True)
+
+class Design(models.Model):
+    """Database entry for a git-repository (possibly pointing to a specific git branch)"""
+    designer = models.ForeignKey(User)
+    name = models.CharField(max_length = 150)
+    short_name = models.SlugField()
+    repo_path = models.CharField(max_length = 80, editable = False, blank = True)
+    branch = models.CharField(max_length = 100, editable = False, blank = True, null = True)
+    parent = models.ForeignKey("Design", null = True, blank = True)
+    derived_from = models.ManyToManyField("Design", related_name = "derivatives")
+    license = models.ForeignKey(License)
+    description = models.TextField(blank = True)
+    instructions = models.TextField(blank = True)
+    created = models.DateTimeField(auto_now_add = True)
+    last_updated = models.DateTimeField(auto_now = True)
+
+class Release(models.Model):
+    """Database entry for a git-tag"""
+    design = models.ForeignKey(Design)
+    name = models.CharField(max_length = 100)
+    date = models.DateTimeField()
+
+class Build(models.Model):
+    """Report of a build of a design by someone"""
+    builder = models.ForeignKey(User)
+    design = models.ForeignKey(Design)
+    date = models.DateTimeField(auto_now_add = True)
+    comment = models.TextField()
+    image = models.ForeignKey(Media)
+
